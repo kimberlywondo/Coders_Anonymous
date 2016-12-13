9e62a1fe77b597b3f6eb7f2375f56e612042ed9c
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2016-12-13 17:55:49 -0500

    Login and posts working

--- app.js
+++ app.js
@@ -4,26 +4,51 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
+var postEntries = require('./routes/postEntries');
 var users = require('./routes/users');
 
 var app = express();
+
+// Connect to database
+mongoose.connect('mongodb://localhost/Coders_Anonymous');//change to mongolab DB
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: '?',
+                  resave: true,
+                  saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport/passport')(passport);
+
+// This middleware will allow us to use the currentUser in our views and routes.
+app.use(function (req, res, next) {
+  global.currentUser = req.user;
+  next();
+});
+
+
 app.use('/', index);
 app.use('/users', users);
+app.use('/postEntries', postEntries);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +67,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+console.log('You made it!');
 
 module.exports = app;
