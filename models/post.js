f1abc65246ef561fb9bcc52ce753131fca50227b
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2016-12-14 13:03:26 -0500

    removed old post schema

--- models/post.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var PostSchema = new mongoose.Schema({
-  title:         { type: String,  required: true },
-  },
-  { timestamps: true }  // createdAt, updatedAt
-);
-
-module.exports = mongoose.model('Post', PostSchema);
