5c5148a90b64e79d8609ea291b63dd9db1b24a58
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2016-12-14 13:04:06 -0500

    added timestamp method to postEntrySchema

---
+++ models/postEntry.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var PostEntrySchema = new mongoose.Schema({
+	title: { type: String, required: true },
+  	text: { type: String },
+  	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, {
+  timestamps: true
+});
+
+function date2String(date) {
+  var options = {
+    weekday: 'long', year: 'numeric', month: 'short',
+    day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
+  };
+  return date.toLocaleDateString('en-US', options);
+}
+
+PostEntrySchema.methods.getCreatedAt = function() {
+  return date2String(this.createdAt);
+};
+
+module.exports = mongoose.model('PostEntry', PostEntrySchema);
