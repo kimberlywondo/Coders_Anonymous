0a70b728d069080894f99f70833dacca9f7d8844
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2016-12-14 17:57:01 -0500

    required mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var mongoose = require('mongoose');
 
 var PostEntry = require('../models/postEntry');
 
