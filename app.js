7b6cb82f448e46a5d58f69aa981eaa6f41a7eff2
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2016-12-15 14:10:18 -0500

    added mongolab DB connection

--- app.js
+++ app.js
@@ -16,8 +16,23 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // Connect to database
-mongoose.connect('mongodb://localhost/Coders_Anonymous');//change to mongolab DB
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
